a48c97bafd73cc4dabe4f9c3acfa15e841bb8db9
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-03-21 15:11:52 +0100

    Add OLDPWD support to cd buildin

--- src/buildin_cd.c
+++ src/buildin_cd.c
@@ -6,12 +6,23 @@
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 17:57:53 by tguillem          #+#    #+#             */
-/*   Updated: 2016/03/19 14:53:10 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/03/21 15:10:06 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int		compute_options(void *data, char *c)
+{
+	char	*params;
+
+	params = (char*)data;
+	if (*c == '\0')
+		*params = 1;
+	else
+		return (0);
+	return (1);
+}
 static void		set_env(t_env *env, char *key, char *value)
 {
 	t_array		*tmp;
@@ -39,12 +50,12 @@ static char		*get_oldpwd(t_env *env)
 	return (old_path);
 }
 
-static char		*getpwd(int ac, char **av, t_env *env)
+static char		*getpwd(char *str, char *params, t_env *env)
 {
 	char	*path;
 	t_array	*tmp;
 
-	if (ac == 1)
+	if (!str)
 	{
 		if (!(tmp = array_get(env->env, "HOME=")))
 		{
@@ -54,7 +65,19 @@ static char		*getpwd(int ac, char **av, t_env *env)
 		path = tmp->data + 5;
 	}
 	else
-		path = av[1];
+	{
+		if (*params == 1)
+		{
+			if (!(tmp = array_get(env->env, "OLDPWD=")))
+			{
+				ft_printf_fd(2, "cd: OLDPWD not set\n");
+				return (NULL);
+			}
+			path = tmp->data + 7;
+		}
+		else
+			path = str;
+	}
 	return (path);
 }
 
@@ -64,13 +87,16 @@ int				minishell_buildin_cd(char **args, t_env *env)
 	int		ac;
 	char	*old_path;
 	char	*path;
+	char	*params;
 
 	if ((ac = char_array_length(args)) > 2)
 		ft_putstr_fd("cd: too many arguments\n", 2);
 	else
 	{
+		params = ft_strnew(3);
+		ft_parse_args(ac, args, params, &compute_options);
 		old_path = get_oldpwd(env);
-		path = getpwd(ac, args, env);
+		path = getpwd(ac == 1 ? NULL : args[1], params, env);
 		if (path && !chdir(path))
 		{
 			set_env(env, "PWD=", path);
@@ -80,6 +106,7 @@ int				minishell_buildin_cd(char **args, t_env *env)
 				ft_strdel(&old_path);
 			}
 		}
+		ft_strdel(&params);
 	}
 	return (1);
 }
